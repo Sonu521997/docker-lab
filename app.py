6c36bb972b2558ed26997bb6791fe8eb9bbc28e9
Sonu521997 <sonu.kmar3865@gmail.com>
2025-01-05 21:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1 style="color:red">Python Flask App </h1>'
+    return '<h1 style="color:red"> hello Guys, Welcome to my course  </h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
